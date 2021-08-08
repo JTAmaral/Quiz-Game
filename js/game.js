aad5400a6fa97f85e70ad46959bc496a0401737c
Jared Amaral <szenseiii@gmail.com>
2021-08-08 12:42:57 -0700

    first commit

---
+++ js/game.js
@@ -0,0 +1,114 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: 'What is 9 + 10?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 3,
+    },
+    {
+        question: 'Who was in Paris?',
+        choice1: 'Nigga',
+        choice2: 'Niggas',
+        choice3: 'Nigas',
+        choice4: 'Niggus',
+        answer: 2,
+    },
+    {
+        question: "Fill in the blank. When a ____ get an attitude.",
+        choice1: 'Nigga',
+        choice2: 'Fag',
+        choice3: 'Austin',
+        choice4: 'Queer',
+        answer: 1,
+    },
+    {
+        question: "Fill in the blank. Mis ______ hablan mucho.",
+        choice1: 'amigros',
+        choice2: 'amigras',
+        choice3: 'anigros',
+        choice4: 'anigras',
+        answer: 2,
+    }
+]
+
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html');
+    }
+
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })
+
+    availableQuestions.splice(questionsIndex, 1);
+
+    acceptingAnswers = true;
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+}
+
+startGame();
+
+
